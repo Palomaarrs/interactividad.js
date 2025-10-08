17ef864d3cfa27636f40112d44ac55ca5ce51080
Paloma <parreola@centro.edu.mx>
2025-10-08 10:57:25 -0600

    variables

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var nombre= 'Paloma';
+let edad= 23;
+const PI= 3.1416;
+const TAU= Math.PI * 2;
+let esMayorDeEdad= edad >= 18;
+nombre= 'Paloma Arreola';
+edad= 24;
+console.log('Hola ' + nombre);
