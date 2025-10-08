4c4b232eb97dd834a8db50888f0750f4f5da8370
Paloma <parreola@centro.edu.mx>
2025-10-08 11:37:02 -0600

    comentarios

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 var nombre= 'Paloma';
+// asi se declara una funcion
 let edad= 23;
 const PI= 3.1416;
 const TAU= Math.PI * 2;
@@ -8,8 +9,11 @@ edad= 24;
 console.log('Hola ' + nombre);
 
 function saludar(_nombre= "Mundo") {
+    // asi se declara patametro de una funcion
     alert("Hola" + _nombre);
 }
-saludar();
+// asi se llama a una funcion
+saludar(); //llamada a una funcion sin parametros
+//llamada a una funcion con parametros
 saludar(nombre);
 saludar("Juan");
