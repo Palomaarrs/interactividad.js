cc767c097519ae05aac9f73d835e6efca572ebd0
Paloma <parreola@centro.edu.mx>
2025-10-13 20:19:05 -0600

    inputs

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,5 +1,16 @@
 const toggleThemeBtn = document.getElementById('toggle-theme-btn');
+const colorPicker = document.getElementById('color-picker');
+const backgroundPicker = document.getElementById('background-picker');
 
 toggleThemeBtn.addEventListener('click', function() {
-    alert("me diste click");
+    // alert("me diste click");
+    document.body.classList.toggle('dark');
 })
+
+colorPicker.addEventListener('change', function() {
+    document.body.style.color = colorPicker.value;
+});
+
+backgroundPicker.addEventListener('change', function() {
+    document.body.style.background = backgroundPicker.value;
+});
