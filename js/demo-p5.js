d0924dc45bd68b93b4432895076ef33bd972df55
Paloma <parreola@centro.edu.mx>
2025-10-27 20:28:52 -0600

    cambiooos

--- js/demo-p5.js
+++ js/demo-p5.js
@@ -12,12 +12,12 @@ function windowResized() {
 function mouseMoved() {
     stroke(0);
     fill(300, 100);
-    const red = Math.abs(Math.sin(frameCount * 0.01) * 255);
-    const green = Math.abs(Math.sin(frameCount * 0.02) * 255);
-    const blue = Math.abs(Math.sin(frameCount * 0.03) * 255);   
-    fill (red, green, blue);
+    //const red = Math.abs(Math.sin(frameCount * 0.01) * 255);
+    //const green = Math.abs(Math.sin(frameCount * 0.02) * 255);
+    //const blue = Math.abs(Math.sin(frameCount * 0.03) * 255);   
+    //fill (red, green, blue);
     const size = Math.sin(frameCount * .01) * 100;
-    //ellipse(mouseX, mouseY, size, size);
-    rect (mouseX, mouseY, size, size);
+    ellipse(mouseX, mouseY, size, size);
+    //rect (mouseX, mouseY, size, size);
     
 }
